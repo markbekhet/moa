a9242ab583758e7e85c770c56db1ad7d27244cca
Mark Bekhet <markonsy72@gmail.com>
2023-02-14 09:42:43 -0500

    Revert "Change to trigger jenkins"
    This reverts commit 930fbf56f1eea22e19b0eb5e3f39aa837ff63fdc.

--- moa/src/main/java/moa/classifiers/functions/MultinomialLogisticRegression.java
+++ moa/src/main/java/moa/classifiers/functions/MultinomialLogisticRegression.java
@@ -20,6 +20,7 @@ public class MultinomialLogisticRegression extends AbstractClassifier  implement
 
     private boolean shouldReset = true;
 
+
     @Override
     public String getPurposeString() {
         return "Logistic regression: A simple logistic regression based classifier.";
